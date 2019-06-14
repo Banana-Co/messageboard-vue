1361a56b79de21660a0c5e93bae704ec3bfa4f51
CastJo <fjchfjch@163.com>
2019-06-14 16:21:17 +0800

    修改首页按钮

--- src/components/common/BlogHeader.vue
+++ src/components/common/BlogHeader.vue
@@ -3,7 +3,7 @@
   <el-row>
 
     <el-col :span="4">
-        <button><img src="@/assets/logo.png" alt="" @click="ToLogin" height="40px"></button>
+        <el-button  type="text"><img src="@/assets/logo.png" alt="" @click="ToLogin" height="40px"></el-button>
     </el-col>
 
     <el-col :span="3" :offset="17">
